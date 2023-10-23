c48efd6ebd2cb09d6f8be5f8683c83a100fd02f7
DanielSan5 <130354244+DanielSan5@users.noreply.github.com>
2023-10-23 10:22:06 +0200

    Add files via upload

--- progetto_max/scripts/script_index.js
+++ progetto_max/scripts/script_index.js
@@ -1,41 +1,70 @@
-
 document.addEventListener('DOMContentLoaded', function() {
-  /*  var infoIcon = document.querySelector('.info-icon');
-    infoIcon.addEventListener('mouseover', function() {
-        infoIcon.style.color = '#ff4500'; 
-    });
-    infoIcon.addEventListener('mouseout', function() {
-        infoIcon.style.color = '#333'; 
-    });*/
 
-    
+    document.getElementById('submit_credenziali').addEventListener("click", function(){
+        event.preventDefault(); // Impediamo il comportamento predefinito del pulsante
+
+       var pass_uno = document.getElementById('password_uno').value;
+       var pass_due = document.getElementById('password_due').value;
+
+       if(pass_uno === 'Emeth') {
+
+            if(pass_due === 'meth') {
+                document.getElementById('error-message').textContent = "";
+                document.getElementById("correct_audio").play(); 
+                setTimeout(function() {
+                    window.location = "autodestruction.html";
+                }, 500); // 1000 millisecondi = 1 secondo
+            }
+       }else{
+            document.getElementById('error-message').textContent = "Le password NON sono corrette";
+           
+            // Svuota i campi delle password
+            document.getElementById("password_uno").value = "";
+            document.getElementById("password_due").value = "";
+
+            document.getElementById("error_audio").play();   
+            
+            // Mostra il messaggio
+            document.getElementById("error-message").style.display = "block";
+
+                setTimeout(function() {
+                    document.getElementById("error-message").style.display = "none";
+                    document.getElementById("error-message").textContent = "";
+                }, 1300); // 1000 millisecondi = 1 secondo
+        }
+
+    })
+
+
 });
+ 
+/*
+let passowrdString = '';
+const passwordInput = document.getElementById('password_uno');
+let inputValue = '';
+let maskedValue = '';
 
-  //funzione che apre il popup
-  function openInfoPopup() {
-        var popup = document.querySelector('.info-popup');
-        var closeBtn = document.createElement('span');
-        closeBtn.className = 'close-popup';
-        closeBtn.innerHTML = '&times;';
-        popup.innerHTML = '<h2>Informazioni sulla pagina</h2><p>Questa è una pagina di accesso di esempio.</p>';
-        popup.appendChild(closeBtn);
-        closeBtn.addEventListener('click', closeInfoPopup);
-        popup.style.display = 'block';
-    }
+    passwordInput.addEventListener('input', function(){
 
-    //funzione che chiude il popup
-    function closeInfoPopup() {
-        var popup = document.querySelector('.info-popup');
-        popup.style.display = 'none';
-    }
-    
-     
-    /*
+        inputValue = passwordInput.value;
 
-    // Imposta un timeout per nascondere il messaggio dopo 5 secondi
-    
-      setTimeout(function () {
-        document.getElementById("error-message").style.display = "none";
-    }, 5000);
+        if(inputValue){
+            var dif = inputValue.length - passowrdString.length;
 
-    */
+            if(dif > 0){
+                passowrdString += inputValue.slice(-1);
+            }else if (dif < 0){
+                passowrdString = passowrdString.slice(0, dif);
+            }
+
+        }else{
+            passowrdString = '';
+        }
+       
+        maskedValue = '*'.repeat(inputValue.length);
+
+        //Sostituisci i caratteri con *
+        passwordInput.value = maskedValue;
+        console.log(passowrdString);
+    })
+*/
