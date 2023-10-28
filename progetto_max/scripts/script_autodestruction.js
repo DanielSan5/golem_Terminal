4964f10c96d93f9cad1a5e691deeb1a3d23c45a0
DanielSan5 <130354244+DanielSan5@users.noreply.github.com>
2023-10-28 12:58:02 +0200

    Add files via upload

--- progetto_max/scripts/script_autodestruction.js
+++ progetto_max/scripts/script_autodestruction.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
+const immagine_cd = document.getElementById("immagine");
+immagine_cd.style.display = "none"; //OSCURO L'IMMAGINE
 
-    
     document.getElementById('submit_yes').addEventListener("click", function(){
 
         //sound di porta che NON si apre
@@ -20,6 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById("autodistruzione").style.display = "none";
             document.getElementById("submit_yes").style.display = "none";
             document.getElementById("submit_no").style.display = "none";
+            
 
             setTimeout(function() {
 
@@ -28,11 +30,12 @@ document.addEventListener('DOMContentLoaded', function() {
                 document.body.style.backgroundColor = "#f5e400";
                 document.getElementById("danger_audio").addEventListener("ended", function(){
                     document.body.style.backgroundColor = "#0a0a0a";
+                   
                     setTimeout(function() {
                         document.getElementById("danger_audio1").play();
                         console.log("secondo");
                         document.body.style.backgroundColor = "#f5e400";
-                    }, 500); // 1000 millisecondi = 1 secondo
+                    }, 360); // 1000 millisecondi = 1 secondo
                 });
 
                 document.getElementById("danger_audio1").addEventListener("ended", function(){
@@ -41,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         document.getElementById("danger_audio2").play();
                         console.log("terzo");
                         document.body.style.backgroundColor = "#f5e400";
-                    }, 500); // 1000 millisecondi = 1 secondo
+                    }, 360); // 1000 millisecondi = 1 secondo
                 });
 
                 
@@ -50,17 +53,72 @@ document.addEventListener('DOMContentLoaded', function() {
                     //Parte l'audio del countdown
                     setTimeout(function() {
                     document.getElementById("countdown").play();
+                        document.getElementById("gif_warning").style.display = "block";
+                            //RIPETERSI DI TIMOUT E CAMBIO DI IMMAGINI PER IL COUNTDOWN
+                            setTimeout(function() {
+                                document.getElementById("gif_warning").style.display = "none";
+                                immagine_cd.style.display = "block"; //VISIONO L'IMMAGINE
+                                immagine_cd.src = "../files/images/ten.jpg";
+                            }, 4166);
+
+                            setTimeout(function() {
+                                immagine_cd.src = "../files/images/nine.jpg";
+                            }, 5881);
+
+                            setTimeout(function() {
+                                immagine_cd.src = "../files/images/eight.jpg";
+                            }, 7024);
+
+                            setTimeout(function() {
+                                immagine_cd.src = "../files/images/seven.jpg";
+                            }, 7892);
+
+                            setTimeout(function() {
+                                immagine_cd.src = "../files/images/six.jpg";
+                            }, 8773);
+
+                            setTimeout(function() {
+                                immagine_cd.src = "../files/images/five.jpg";
+                            }, 9884);
+
+                            setTimeout(function() {
+                                immagine_cd.src = "../files/images/four.jpg";
+                            }, 10905);
+
+                            setTimeout(function() {
+                                immagine_cd.src = "../files/images/three.jpg";
+                            }, 11916);
+
+                            setTimeout(function() {
+                                immagine_cd.src = "../files/images/two.jpg";
+                            }, 13087);
+
+                            setTimeout(function() {
+                                immagine_cd.src = "../files/images/one.jpg";
+                            }, 14128);
+
+                            setTimeout(function() {
+                                immagine_cd.src = "../files/images/zero.jpg";
+                            }, 15421);
 
                     //Quando finisce parte l'audio dell'esplosione
                         document.getElementById("countdown").addEventListener("ended", function(){
-                            document.getElementById("explosion").play();
+                            immagine_cd.style.display = "none";
+                            document.getElementById("explosion_sound").play();
+                            document.body.style.backgroundColor = "#ffffff";
+                            document.getElementById("explosion_img").style.display = "block";
                         });
+
+                        document.getElementById("explosion_sound").addEventListener("ended", function(){
+                            document.getElementById("explosion_img").style.display = "none";
+                            document.getElementById("victory").style.display = "block";
+                        })
 
                     }, 200); // 1000 millisecondi = 1 secondo
 
                 });                            
         
-            }, 500); // 1000 millisecondi = 1 secondo
+            }, 360); // 1000 millisecondi = 1 secondo
 
         }) //TUTTO QUESTO QUANDO è TERMINATO L'AUDIO DELLA PORTA CHE SI APRE     
     
